e6db3dd11a55b4b8f2f9882c1738880f1d35d907
Xingyu Wang <brandon.xyw@gmail.com>
2019-09-23 13:46:56 -0400

    draw 8 teapots, change camera position

--- MultiObjects.cpp
+++ MultiObjects.cpp
@@ -9,16 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void draw() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();  // reset the current matrix to identity matrix
-
-	float eye[] = { 0, 0, 8 };
-	float center[] = { 0, 0, 0 };
-	// create a viewing matrix defined by an eye point and a reference point
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	glEnable(GL_DEPTH_TEST);
+void init() {
+    glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
 	GLfloat light_pos[] = { 5, 5, 5, 1 };
@@ -27,9 +19,56 @@ void draw() {
 	glLightfv(GL_LIGHT0, GL_AMBIENT, white);  // specify the ambient RGBA intensity of the light
 	glEnable(GL_LIGHT0);
 
-	glutSolidTeapot(.5);  // draw the teapot
+}
 
-	glutSwapBuffers();
+void draw_eight_objs() {
+    glPushMatrix();
+    glTranslatef(-0.3f, 0.3f, -0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.3f, 0.3f, -0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.3f, -0.3f, -0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.3f, -0.3f, -0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.3f, 0.3f, 0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.3f, 0.3f, 0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.3f, -0.3f, 0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0.3f, -0.3f, 0.3f);
+    glutSolidTeapot(.1);  // draw the teapot
+    glPopMatrix();
+}
+
+void draw() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();  // reset the current matrix to identity matrix
+
+	// create a viewing matrix defined by an eye point and a reference point
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
 	glMatrixMode(GL_PROJECTION);
@@ -37,7 +76,11 @@ void draw() {
 	gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // define a viewing matrix
+	gluLookAt(0.0, 0.0, 1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // define a viewing matrix
+
+    draw_eight_objs();
+
+    glutSwapBuffers();
 }
 
 int main(int argc, char *argv[])
@@ -46,7 +89,9 @@ int main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
 	glutInitWindowSize(480, 480);  // define size of the window
 	glutInitWindowPosition(0, 0);  // define position of the window
-	glutCreateWindow("Hello teapot");
+	glutCreateWindow("Multi_Object");
+
+    init();
 
 	glutDisplayFunc(draw);  // display callback
 	glutMainLoop();  // enter event processing loop
