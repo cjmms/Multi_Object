3c7f050039b5f24050a6069bd6d1df505460e9f7
Xingyu Wang <brandon.xyw@gmail.com>
2019-09-24 12:04:45 -0400

    color

--- MultiObjects.cpp
+++ MultiObjects.cpp
@@ -9,18 +9,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+GLfloat** material;
+
 void init() {
     glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-    glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	//glEnable(GL_LIGHTING);
+    // glEnable(GL_BLEND);
+	// glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+    // glEnable(GL_COLOR_MATERIAL);
 
-	GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat white[] = { 0.2, 0.2, 0.2, 1.0 };
 	GLfloat light_pos[] = { 5, 5, 5, 1 };
 
-	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);  // define the position of the light
-	glLightfv(GL_LIGHT0, GL_AMBIENT, white);  // specify the ambient RGBA intensity of the light
-	glEnable(GL_LIGHT0);
+	//glLightfv(GL_LIGHT0, GL_POSITION, light_pos);  // define the position of the light
+	//glLightfv(GL_LIGHT0, GL_AMBIENT, white);  // specify the ambient RGBA intensity of the light
+	//glLightfv(GL_LIGHT0, GL_DIFFUSE, white);  // specify the ambient RGBA intensity of the light
+	//glEnable(GL_LIGHT0);
+
+    glShadeModel(GL_SMOOTH);
+
+    glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // define a viewing matrix
 }
 
 // x axis is red, y axis is green, z axis is blue
@@ -48,7 +63,8 @@ void drawThreeAxises () {
 void drawTeapot(double x, double y, double z) {
     glPushMatrix();
     glTranslatef(x, y, z);
-    //glColor3f(1.0, 0.0, 0.0);
+    //glEnable(GL_COLOR_MATERIAL);
+    glColor3f(0.85, 0.48, 0.47);
     glutSolidTeapot(.08);  // draw the teapot
     drawThreeAxises();
     glPopMatrix();
@@ -67,18 +83,6 @@ void draw_eight_objs() {
 
 void draw() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();  // reset the current matrix to identity matrix
-
-	// create a viewing matrix defined by an eye point and a reference point
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	glViewport(0, 0, 480, 480);  // define a viewport and display in this viewport
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(60.0, 1, 1.0, 20.0);  // specify a viewing frustum
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0.0, 0.0, 1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // define a viewing matrix
 
     gluSphere(gluNewQuadric(), 0.07f, 32, 32);
 
